b1bbb8356b1759770b0ad5a5cd1ad991570b4020
Ben <whippedcreamtoast@gmail.com>
2023-11-12 19:46:23 -0500

    created models

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class Message(models.Model):
+    message_text = models.CharField(max_length=200)
+
+class User(AbstractUser):
+    pass
+
+class ChatRoom(models.Model):
+    room_name = models.CharField(max_length=50)
