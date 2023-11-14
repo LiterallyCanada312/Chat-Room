77bb451287488ea8158c0456014b7e5aa0ee8665
Ben <whippedcreamtoast@gmail.com>
2023-11-13 19:16:31 -0500

    started channels

--- mysite/Messaging/models.py
+++ mysite/Messaging/models.py
@@ -4,6 +4,8 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class Message(models.Model):
     message_text = models.CharField(max_length=200)
+    time_sent = models.DateTimeField()
+    user_sent = models.User() 
 
 class User(AbstractUser):
     pass
