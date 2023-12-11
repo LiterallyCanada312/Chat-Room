5ebf09098cf8c691e25b580201024b5fab38d155
Ben <whippedcreamtoast@gmail.com>
2023-12-10 19:50:21 -0500

    functionality n shit

--- chat/consumers.py
+++ chat/consumers.py
@@ -2,14 +2,41 @@
 
 import json
 from channels.generic.websocket import WebsocketConsumer
+
+
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
-        self.accept()
+        self.room_id = self.scope['url_route']['kwargs']['room-id']
+        self.room_group_name = f'chat_{self.room_id}'
+
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+
+
     def disconnect(self, close_code):
-        pass
-    def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json['message']
-        self.send(text_data=json.dumps({
-            'message': message
-        }))
+
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
+
+    async def receive(self, text_data):
+        sender = self.scope['user'].username
+        message = text_data.strip()
+
+        chat_room = ChatRoom.objects.get(id=self.room_id)
+        message = Messages.objects.create(room=chat_room, sender=self.scope['user'], content = message)
+
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type' : 'send_message',
+                'message' : f'{sender}: {message.content}'
+            }
+        )
+    
+    def send_message(self, text_data):
+        await self.send(text_data=event['message'])
+
--- chat/routing.py
+++ chat/routing.py
@@ -1,5 +1,5 @@
 from django.urls import re_path
 
 websocket_urlpatterns = [
-    re_path(r'ws/chat/(?P<room_name>\w+)$', consumers.ChatRoomConsumer),
+    re_path(r'ws/chat/(?P<room_name>\w+)$', ChatConsumer.as_asgi()),
 ]
