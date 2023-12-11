c911402d2e85ec1f4e4182c3b689407f4809667e
Ben <whippedcreamtoast@gmail.com>
2023-12-10 19:30:33 -0500

    models n shit

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class ChatRoom(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__self(self):
+        return self.name
+    
+
+class Message(models.Model):
+    room = models.ForeignKey(ChatRoom, on_delete=models.CASCADE)
+    sender = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.content
+    
+
